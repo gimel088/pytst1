bba662de07a74baa2574937844b805b9315354b3
Gimel <gimel088@gmail.com>
2018-06-13 17:43:03 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# pytst1
+* PyTst1 *
+docker image using alpine and flask.
---
+++ dock1/app.py
@@ -0,0 +1,29 @@
+#tst1
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello! here is flsk app ' + __name__
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'flask User %s' % username
+
+@app.route('/page/<pageon>')
+def forurl(pageon):
+    # show the static page URL
+    u = request.cookies.get('username')
+    
+    return 'Page %s' % Flask.static_url_path + pageon
+
+@app.route('/args')
+def show_args():
+    m = request.method
+    return 'method:' + repr(m) + '. args:' + repr(request.args)
+
+if __name__ == '__main__':
+    app.run('0.0.0.0',3002)
